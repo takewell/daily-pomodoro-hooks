051e42afcdaadf8c8b559c8a1d9c93c7fa061286
yuya takei <takewell.dev@gmail.com>
2019-06-30 00:38:32 +0900

    Update page layout

---
+++ src/components/PomodoroTimer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './PomodoroTimer.css'
+
+export const PomodoroTimer: React.FC = () => {
+  return <div className="pomodoro-timer">pomodorotimer</div>;
+};
---
+++ src/components/TaskKeeper.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TaskKeeper.css'
+
+export const TaskKeeper: React.FC = () => {
+  return <div className="task-keeper">
+    createTask
+  </div>
+};
