527d5f3e13da3faf1a038d82e09303106712c464
yuya takei <takewell.dev@gmail.com>
2019-07-14 02:09:15 +0900

    Feat TaskKeper

--- src/components/TaskKeeper.tsx
+++ src/components/TaskKeeper.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import './TaskKeeper.css'
+import React, { useState, KeyboardEvent } from "react";
+import "./TaskKeeper.css";
 
 export const TaskKeeper: React.FC = () => {
-  return <div className="task-keeper">
-    createTask
-  </div>
+  const keepInitState: { id: number; title: string; text: string }[] = [];
+  const [keeps, setKeeps] = useState(keepInitState);
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13 && e.metaKey) {
+      e.preventDefault();
+      setKeeps([
+        ...keeps,
+        {
+          id: keeps.length,
+          title: e.currentTarget.value,
+          text: ""
+        }
+      ]);
+      e.currentTarget.value = "";
+    }
+  };
+
+  return (
+    <div className="task-keeper">
+      <input
+        className="create-keeptask"
+        type="text"
+        placeholder="input memo"
+        onKeyDown={onKeyDown}
+      />
+      {keeps.map((e, i) => (
+        <div key={i} className="keep">
+          <h2>{e.title}</h2>
+          <p>{e.text}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
